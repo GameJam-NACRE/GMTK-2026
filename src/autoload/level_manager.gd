9e88af21b4cedc3993849a300c025bb866be3d77
ficeloo <crostheophile@gmail.com>
2026-07-23 01:28:51 +0200

    ajout autoload : managers + mv event_bus

---
+++ src/autoload/level_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@onready var level_container: Node
+
+var current_level: Level = null
+
+func load_level(level_scene: PackedScene):
+	if current_level:
+		current_level.queue_free()
+
+	current_level = level_scene.instantiate()
+	level_container.add_child(current_level)
+	EventBus.level_loaded.emit()
