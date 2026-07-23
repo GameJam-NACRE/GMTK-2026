c5f31782287ed0369c5bcb833b3d97124cb13902
ficeloo <crostheophile@gmail.com>
2026-07-23 17:59:23 +0200

    modif code en vif

--- src/levels/components/flag_area.gd
+++ src/levels/components/flag_area.gd
@@ -5,7 +5,6 @@ var _triggered = false
 
 func _ready() -> void:
 	self.add_to_group("level_component")
-	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("player"):
@@ -15,4 +14,3 @@ func _on_body_entered(body: Node2D) -> void:
 	
 	_triggered = true
 	EventBus.flag_reached.emit()
-
