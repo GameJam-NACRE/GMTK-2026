84f2b07647c8ac7fdb1de5095c892cbe795ca204
slink7 <slink794@gmail.com>
2026-07-23 14:07:42 +0200

    :sparkles: Add basic character mecanics

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,38 +1,25 @@
 extends CharacterBody2D
 
-@export var speed = 300.0
-@export var jump_velocity = -400.0
-@onready var sprite = $Sprite2D
 
-@export var idle_texture: Texture2D
-@export var walk_texture: Texture2D
-@export var jump_texture: Texture2D
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-var screen_size
-
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func _ready():
-	screen_size = get_viewport_rect().size
 
 func _physics_process(delta: float) -> void:
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("move_up") and is_on_floor():
-		velocity.y = jump_velocity 
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("move_left", "move_right")
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * speed 
+		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-	if not is_on_floor():
-		$AnimatedSprite2D.play("jump")
-	elif direction != 0:
-		$AnimatedSprite2D.play("walk")
-	else:
-		$AnimatedSprite2D.flip_h = direction < 0
