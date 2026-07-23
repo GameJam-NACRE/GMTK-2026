2688e34e20ef78b638cbc837820fca420f5dfdaa
ficeloo <crostheophile@gmail.com>
2026-07-23 18:15:39 +0200

    ajout attente de de intro cd

--- main.gd
+++ main.gd
@@ -2,4 +2,7 @@ extends Node
 
 func _ready() -> void:
 	LevelManager.level_container = $LevelContainer
+	EventBus.intro_countdown_end.connect(_on_intro_countdown_end)
+
+func _on_intro_countdown_end() -> void:
 	GameManager.next_level()
