cec548b24ae66dd0ec3743751387e52f07c3ef71
Noe Bacconnet <noe@bacconnet.com>
2026-07-25 00:27:54 +0200

    add phase 3 clicker

--- src/autoload/game_manager.gd
+++ src/autoload/game_manager.gd
@@ -3,8 +3,7 @@ extends Node
 var current_level: int = -1
 
 var level_queue: Array[PackedScene] = [
-	preload("res://scenes/levels/phase0/level_0.tscn"),
-	preload("res://scenes/levels/phase0/level_0.tscn"),
+	preload("res://scenes/levels/phase3/clicker.tscn"),
 	]
 
 func _ready() -> void:
@@ -16,6 +15,11 @@ func next_level() -> void :
 		push_warning("FIN DES NIVEAUX")
 		return
 	push_warning("level %d is loading" % [current_level])
+	if "clicker" in level_queue[current_level].resource_path:
+		EventBus.countdown_clicker_mode.emit()
+	else:
+		EventBus.countdown_hud_mode.emit()
+
 	LevelManager.load_level(level_queue[current_level])
 
 func _on_level_ended() -> void:
--- src/countdown/countdown.gd
+++ src/countdown/countdown.gd
@@ -9,11 +9,19 @@ extends CanvasLayer
 @export var intro_end_font_size: int = 20
 @export var main_fade_in_time: float = 0.1
 
+@export var hud_mode_fade_time: float = 1.0
+@export var clicker_mode_fade_time: float = 1.0
+
+@export var critic_countdown: float = 20.0
 
 @onready var panel_container: PanelContainer = $PanelContainer
 @onready var main_label: Label = $PanelContainer/Label
 @onready var intro_label: Label = $IntroLabel
 @onready var countdown: Timer = $Timer
+@onready var clicker_countdown: VBoxContainer = $CenterContainer/ClickerCountdown
+@onready var clicker_label: Label = $CenterContainer/ClickerCountdown/ClickerLabel
+
+var hud_mode_on: bool = true
 
 const TimeEffectScene = preload("res://scenes/countdown/time_effect.tscn")
 
@@ -22,6 +30,9 @@ func _ready() -> void:
 	countdown.timeout.connect(_on_countdown_timeout)
 	EventBus.add_time.connect(_on_add_time)
 	EventBus.remove_time.connect(_on_remove_time)
+	EventBus.countdown_clicker_mode.connect(_set_mode_clicker)
+	EventBus.countdown_hud_mode.connect(_set_mode_hud)
+	EventBus.stop_countdown.connect(_on_stop_countdown)
 
 	countdown.start(countdown_start + intro_fade_in_time + intro_stay_time + intro_fade_out_time + main_fade_in_time)
 	play_intro_sequence()
@@ -38,6 +49,10 @@ func _process(_delta: float) -> void:
 
 	main_label.text = formated_countdown
 	intro_label.text = formated_countdown
+	clicker_label.text = formated_countdown
+
+	if time_left <= critic_countdown:
+		countdown.set_paused(false)
 
 func play_intro_sequence() -> void:
 	panel_container.modulate.a = 0.0
@@ -59,11 +74,30 @@ func play_intro_sequence() -> void:
 
 	tween.chain().tween_property(panel_container, "modulate:a", 1.0, main_fade_in_time)
 
+func _on_stop_countdown() -> void:
+	countdown.set_paused(true)
 
 func _on_countdown_timeout() -> void:
 	main_label.text = "00:00:00"
 	EventBus.countdown_end.emit()
 	print("Game Over")
+
+func _set_mode_hud() -> void:
+	if not hud_mode_on:
+		var tween = create_tween()
+
+		tween.tween_property(panel_container, "modulate:a", 1.0, hud_mode_fade_time)
+		tween.parallel().tween_property(clicker_countdown, "modulate:a", 0.0, clicker_mode_fade_time)
+		hud_mode_on = true
+
+func _set_mode_clicker() -> void:
+	if hud_mode_on:
+		var tween = create_tween()
+
+		tween.tween_property(panel_container, "modulate:a", 0.0, hud_mode_fade_time)
+		tween.parallel().tween_property(clicker_countdown, "modulate:a", 1.0, clicker_mode_fade_time)
+		hud_mode_on = false
+
 
 func _on_add_time(sec: int) -> void:
 	if countdown.is_stopped():
@@ -90,8 +124,12 @@ func _on_remove_time(sec: int) -> void:
 
 func _create_pop_up_effect(amount: int) -> void:
 	var effect = TimeEffectScene.instantiate()
-	main_label.add_child(effect)
-	effect.global_position = panel_container.global_position + (panel_container.size / 2.0)
+	if hud_mode_on:
+		main_label.add_child(effect)
+		effect.global_position = panel_container.global_position + (panel_container.size / 2.0)
+	else:
+		clicker_label.add_child(effect)
+		effect.global_position = clicker_countdown.global_position + (clicker_countdown.size / 2.0)
 	effect.start(amount)
 
 
