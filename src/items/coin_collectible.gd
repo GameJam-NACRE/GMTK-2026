63608889ded63057c1539aff7f0d2c2efc16ae3c
Zalphux <cyprien.vegnant@gmail.com>
2026-07-26 16:58:46 +0200

    Level 6 fusionné avec le 7

--- src/items/coin_collectible.gd
+++ src/items/coin_collectible.gd
@@ -11,15 +11,5 @@ func _on_body_entered(body: Node2D) -> void:
 	item_effect()
 	queue_free()
 
-func _on_body_entered(body: Node2D) -> void:
-	print("collected")
-	if not body.is_in_group("player"):
-		return
-	if is_collected == true:
-		return
-
-	item_effect()
-	queue_free()
-
 func item_effect() -> void:
 	EventBus.add_coin.emit()
