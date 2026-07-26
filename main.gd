8a2e3c7c3bb533b878e3144f99702d16d3d8f86c
ficeloo <crostheophile@gmail.com>
2026-07-26 17:52:59 +0200

    fix last minute

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node
 func _ready() -> void:
 	LevelManager.level_container = $LevelContainer
 	EventBus.intro_countdown_end.connect(_on_intro_countdown_end)
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 
 func _on_intro_countdown_end() -> void:
 	GameManager.next_level()
